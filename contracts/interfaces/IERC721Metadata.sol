976a96e348cd7cc94197f8d877a1ec61064b612f
turquito70 <113914568+turquito70@users.noreply.github.com>
2024-07-17 19:56:47 -0400

    IERC721Metadata.sol

--- contracts/interfaces/IERC721Metadata.sol
+++ contracts/interfaces/IERC721Metadata.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (interfaces/IERC721Metadata.sol)
 
-pragma solidity ^0.8.20;
+/**
+* pragma solidity ^0.8.20;
+* Change to:
+*/
+pragma solidity >=0.8.0;
 
-import {IERC721Metadata} from "../token/ERC721/extensions/IERC721Metadata.sol";
+/**
+*    import {IERC721Metadata} from "../token/ERC721/extensions/IERC721Metadata.sol";
+* Change to:
+*/
+import {IERC721Metadata} from "https://github.com/turquito70/openzeppelin-contracts/edit/master/contracts/interfaces/extensions/IERC721Metadata.sol";
