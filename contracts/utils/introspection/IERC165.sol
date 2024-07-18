9170d22ec32518321f449e9d98ef0b3c9b0ef5d8
turquito70 <113914568+turquito70@users.noreply.github.com>
2024-07-17 20:02:52 -0400

    IERC165.sol

--- contracts/utils/introspection/IERC165.sol
+++ contracts/utils/introspection/IERC165.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (utils/introspection/IERC165.sol)
 
-pragma solidity ^0.8.20;
+/**
+* pragma solidity ^0.8.20;
+* Change to:
+*/
+pragma solidity >=0.8.0;
 
 /**
  * @dev Interface of the ERC-165 standard, as defined in the
